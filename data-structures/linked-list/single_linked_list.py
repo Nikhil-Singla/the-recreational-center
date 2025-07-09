e2799de8cae26fb5c7055e7ee14979fda3bbeb47
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-07-09 14:58:37 -0500

    Initializing linkedlist attempt

---
+++ data-structures/linked-list/single_linked_list.py
@@ -0,0 +1,11 @@
+class SingleLinkedList:
+  def __init__(self, value=None, next=None, list=None) -> None:
+    self.value = value
+    self.__next = next
+    self.__list = list
+
+def main() -> None:
+  pass
+
+if __name__ == '__main__':
+  main()
