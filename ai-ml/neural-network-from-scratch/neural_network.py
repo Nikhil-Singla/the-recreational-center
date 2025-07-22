f95abc3bf3f5c5a5c81c18d59039724eb79af736
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-07-21 23:29:31 -0700

    Creating output layer for the neuron

--- ai-ml/neural-network-from-scratch/neural_network.py
+++ ai-ml/neural-network-from-scratch/neural_network.py
@@ -5,20 +5,28 @@ import random
 
 random.seed(1)  # Fixing seed to ensure replicability.
 
-n = 5 # This is the number of neurons that input in our target neuron. Each i'th neuron has an input value, along with an associated weight.
-input_range = 10  # Sets the max range of inputs, from -10 to 10, inclusive. 
-weights_range = 5 # Sets the max range of wegihts, from -5 to 5, inclusive. 
+# Configuration: number of inputs, and value ranges for inputs and weights
+n = 5
+input_range = 10
+weights_range = 5
 
-# CURRENT NEURON
-inputs = [round(random.uniform(-1, 1) * input_range, 2) for _ in range(n) ] # Random inputs, upto 2 decimal places between 0 and 10
-weights = [round(random.uniform(-1, 1) * weights_range, 2) for _ in range(n)] # Random weights, upto 2 decimal places between 0 and 5
-bias = random.randint(0, 10)    # Random bias between 0 - 10 inclusive for the entire neuron as a whole
+# ONE NEURON: Generate random inputs, weights and bias in the specified ranges
+inputs = [round(random.uniform(-1, 1) * input_range, 2) for _ in range(n) ]
+weights = [round(random.uniform(-1, 1) * weights_range, 2) for _ in range(n)]
+bias = random.randint(0, 10)
 
-# Modelling output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + inputs[4]*weights[4] + bias
-neuron_result = [i*w for i, w in zip(inputs, weights)]  # Multiply each of the inputs with their corresponding weights
-output = sum(neuron_result) + bias  # Add each of the weighted inputs, along with the neuron bias to the final output of the neuron
+# Remaining 2 neurons in the 3 NEURON LAYER, that get the same inputs but have different sets of weights and biases to the neurons. 
+weights2 = [round(random.uniform(-1, 1) * weights_range, 2) for _ in range(n)]
+weights3 = [round(random.uniform(-1, 1) * weights_range, 2) for _ in range(n)]
+bias2 = random.randint(0, 10)
+bias3 = random.randint(0, 10)
 
+# Modelling output
 
+output = [sum([i*w for i, w in zip(inputs, weights)]) + bias,
+          sum([i*w for i, w in zip(inputs, weights2)]) + bias2,
+          sum([i*w for i, w in zip(inputs, weights3)]) + bias3
+          ]
 
 # XXXXXXXX TESTING OUTPUT ON CONSOLE XXXXXXXX
 # print(inputs, weights) # Checking inputs and weights.
