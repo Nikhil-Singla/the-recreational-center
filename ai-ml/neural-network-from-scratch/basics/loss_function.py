da530ba428f9817447b9bc75831cab196a1e2d67
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-27 16:30:10 -0700

    [I-30] [EXAMPLE] Forward function for CCE Loss

--- ai-ml/neural-network-from-scratch/basics/loss_function.py
+++ ai-ml/neural-network-from-scratch/basics/loss_function.py
@@ -1,5 +1,6 @@
 # Reference: https://numpy.org/doc/stable/reference/routines.math.html
 # Author: Nikhil Singla
+
 import numpy as np
 
 class LossFunction:
@@ -10,6 +11,38 @@ class LossFunction:
         
         return average_data_loss
     
-class Categorical_Entropy_Loss(LossFunction):
+class CategoricalCrossEntropy_Loss(LossFunction):
     def forward(self, y_predicted, y_true):
-        pass
+
+        # Predicted values is an n x m array, where n is the samples, and m is the categories. 
+        # pred[n][m] is the confidence predicted for the mth category in the nth sample
+        # True values is either a one hot encoded or scalar array. 
+        # Case 1 is n x m array, and Case 2 is an array of length n.
+
+
+        # Get total number of samples inputted.
+        # We will use to implement categorical error
+        number_of_elements = len(y_predicted)
+
+        # y_predicted = np.clip(y_predicted, 1e-7, 1-1e-7) We can clip if we like
+        # I prefer to add an epsilon factor to the prediction.
+        # This doesn't solve overconfident predictions, but helps better detect 0 errors.
+        y_predicted[y_predicted == 0] = 1e-16
+
+        # One hot encoded = [[0, 1], [1, 0]]. Implies we are given all the categories and their confidence.
+        # Scalar Values = [0, 1]. Implies we are given the category that is correct.
+        np.mean(-np.log(range(number_of_elements), y_true))
+
+        if len(y_true.shape) == 1:
+            # If we have scalar predictions
+            # For each element, we take the ith element where i E y_true.
+            # This means that range goes through each of the n terms, and y_true is the correct category.             
+            correct_predictions = y_predicted[range(number_of_elements), y_true]
+        
+        if len(y_true.shape) == 2:
+            # Here, we instead multiply the two matrices and their relative terms.
+            # After this, we add all the terms together. In either case, since we assume 
+            # that the other categories are 0, we are only left with the correct predictions. 
+            correct_predictions = np.sum(y_predicted*y_true, axis=1)
+
+        
