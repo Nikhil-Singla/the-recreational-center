f529459ddc09dcc5e4728f5c022f6d979bb49760
Nikhil Singla <nsingla3.14@gmail.com>
2025-11-27 17:24:16 -0600

    [002] Correct some usage and implementation steps.
    Fix timer decorator implementation and enhance brew_tea function.

--- understanding-languages/python/decorators.py
+++ understanding-languages/python/decorators.py
@@ -1,12 +1,22 @@
+import time
+
 # Purpose of decorators is to enhance the base function and then return the enhanced functions.
 
 def timer_decorater(base_func):
-  # Code to decorate.
+  
+  def enhanced_function:
+    start_time = time.timer()
+    base_func()
+    end_time = time.stop()
+    print(f"Task time: {end_time-start_time}" seconds)
+  
   return enhanced_func
 
 
 # @<decorator name> before a function, lets you enhance a base function without modifying its purpose. Helps in decoupling
-# and separating usecases.
-
+# and applying the single responsibility rule to make the code reuasble.
+@timer_decorator
 def brew_tea():
-  # Code to brew.
+  print("Brewing Tea")
+  time.sleep(1)
+  print("Tea finished brewing")
